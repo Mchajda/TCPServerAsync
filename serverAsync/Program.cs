4ca74163390329b82795ccc976e73f795eceabd7
Maciej <48365963+Mchajda@users.noreply.github.com>
2020-11-04 16:50:51 +0100

    init

---
+++ serverAsync/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Server;
+
+namespace serverAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerEchoAPM server = new ServerEchoAPM(new System.Net.IPAddress(new byte[] { 127, 0, 0, 1 }), 2311);
+            server.Start();
+        }
+    }
+}
