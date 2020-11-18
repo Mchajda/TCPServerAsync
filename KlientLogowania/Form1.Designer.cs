6277f30c812727e3c005a4b4e5b85bea130280c2
Vidugavia <49245323+Vidugavia@users.noreply.github.com>
2020-11-18 12:48:52 +0100

    Formularz logowania i rejestracji gotowy. Następny krok: połączenie formularza z serwerem.

--- KlientLogowania/Form1.Designer.cs
+++ KlientLogowania/Form1.Designer.cs
@@ -37,6 +37,20 @@ namespace KlientLogowania
             this.button2 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -48,6 +62,7 @@ namespace KlientLogowania
             this.label1.Size = new System.Drawing.Size(44, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Login";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
@@ -58,6 +73,7 @@ namespace KlientLogowania
             this.label2.Size = new System.Drawing.Size(75, 18);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // textBox1
             // 
@@ -67,6 +83,7 @@ namespace KlientLogowania
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(258, 25);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -76,6 +93,7 @@ namespace KlientLogowania
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(258, 25);
             this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button1
             // 
@@ -86,6 +104,7 @@ namespace KlientLogowania
             this.button1.TabIndex = 4;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -103,12 +122,13 @@ namespace KlientLogowania
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(122, 52);
+            this.label3.Location = new System.Drawing.Point(11, 52);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(46, 18);
             this.label3.TabIndex = 6;
             this.label3.Text = "label3";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
@@ -119,20 +139,192 @@ namespace KlientLogowania
             this.label4.Size = new System.Drawing.Size(259, 25);
             this.label4.TabIndex = 7;
             this.label4.Text = "LOG IN TO THE SYSTEM";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(151, 95);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 21);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Generate password";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(11, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 18);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "label5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(14, 233);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(260, 34);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Submit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.Location = new System.Drawing.Point(14, 118);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(260, 25);
+            this.textBox3.TabIndex = 18;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox4.Location = new System.Drawing.Point(14, 64);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(260, 25);
+            this.textBox4.TabIndex = 17;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(11, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 18);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "New password";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(11, 43);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 18);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "New login";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(23, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(236, 25);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "CREATE A NEW USER";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(11, 209);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(176, 18);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Already have an account?";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(196, 209);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(78, 21);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "Log in";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.ForeColor = System.Drawing.Color.YellowGreen;
+            this.label10.Location = new System.Drawing.Point(12, 43);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(262, 36);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "You have successfully created a new account";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label11.Location = new System.Drawing.Point(11, 146);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(161, 18);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "Confirm new password";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox5.Location = new System.Drawing.Point(14, 167);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(260, 25);
+            this.textBox5.TabIndex = 27;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label12.ForeColor = System.Drawing.Color.Red;
+            this.label12.Location = new System.Drawing.Point(220, 43);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(54, 18);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "label12";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 281);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label10);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Zaloguj się";
@@ -152,6 +344,20 @@ namespace KlientLogowania
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label12;
     }
 }
 
--- KlientLogowania/Form1.cs
+++ KlientLogowania/Form1.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,15 +14,109 @@ namespace KlientLogowania
 {
     public partial class Form1 : Form
     {
-        Form2 register;
-        ServerEchoAPM server;
+        TcpClient client;
+        NetworkStream stream;
+
+        private void HideRegister()
+        {
+            label5.Hide();
+            label6.Hide();
+            label7.Hide();
+            label8.Hide();
+            label9.Hide();
+            label11.Hide();
+            label12.Hide();
+            textBox3.Hide();
+            textBox4.Hide();
+            textBox5.Hide();
+            button3.Hide();
+            button4.Hide();
+            button5.Hide();
+        }
+
+        private void HideLogIn()
+        {
+            label1.Hide();
+            label2.Hide();
+            label3.Hide();
+            label4.Hide();
+            textBox1.Hide();
+            textBox2.Hide();
+            button1.Hide();
+            button2.Hide();
+        }
+
+        private void ShowRegister()
+        {
+            label6.Show();
+            label7.Show();
+            label8.Show();
+            label9.Show();
+            label11.Show();
+            textBox3.Show();
+            textBox4.Show();
+            textBox5.Show();
+            button3.Show();
+            button4.Show();
+            button5.Show();
+        }
+
+        private void ShowLogIn()
+        {
+            label1.Show();
+            label2.Show();
+            label4.Show();
+            textBox1.Show();
+            textBox2.Show();
+            button1.Show();
+            button2.Show();
+        }
+        
+
+        private void OpenRegister()
+        {
+            //Hide log in form
+            HideLogIn();
+
+            label5.Hide();
+            //Show register form
+            ShowRegister();
+
+            //Clear textboxes
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+
+        private void OpenLogin()
+        {
+            label3.Hide();
+            //Show log in form
+            ShowLogIn();
+
+            //Hide register form
+            HideRegister();
+
+            //Clear textboxes
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
 
         public Form1()
         {
             InitializeComponent();
-            register = new Form2(this);
+            
             label3.Text = "";
-            server = new ServerEchoAPM(new System.Net.IPAddress(new byte[] { 127, 0, 0, 1 }), 2311);
+            label5.Text = "";
+            label12.Text = "";
+            
+            client = new TcpClient("localhost", 2311);
+            client.SendBufferSize = 1024;
+            client.ReceiveBufferSize = 1024;
+            stream = client.GetStream();
+
+            //Hide register form
+            HideRegister();
+            label10.Hide();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -31,8 +126,129 @@ namespace KlientLogowania
 
         private void button2_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            register.Show();
+            label10.Hide();
+            OpenRegister();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            //This logs in the user
+            if (textBox1.TextLength != 0 && textBox2.TextLength != 0)
+            {
+                //Username and password are typed
+            }
+            else
+            {
+                label3.Show();
+                label3.Text = "Type an username and a password";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //This submits the register form - must check if any field is empty
+            if (textBox4.TextLength != 0 && textBox3.TextLength != 0)
+            {
+                stream.Write(Encoding.ASCII.GetBytes(textBox4.Text), 0, client.SendBufferSize);
+
+                label10.Show();
+                OpenLogin();
+            }
+            else
+            {
+                label5.Show();
+                label5.Text = "Type an username and a password";
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenLogin();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
